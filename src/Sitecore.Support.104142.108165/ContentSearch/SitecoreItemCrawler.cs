11e1544edd80ceeb4c52990ea5ee5c236bd98279
DK\psk <psk@sitecore.net>
2017-07-27 15:12:15 -0400

    Fix for 104142 also known as 108165

---
+++ src/Sitecore.Support.104142.108165/ContentSearch/SitecoreItemCrawler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace Sitecore.Support.ContentSearch
+{
+    using Sitecore.ContentSearch;
+    public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
+    {
+        protected ConcurrentDictionary<IIndexableUniqueId, object> Processed;
+        public override void Update(IProviderUpdateContext context, IIndexableUniqueId indexableUniqueId,
+            IndexEntryOperationContext operationContext, IndexingOptions indexingOptions = IndexingOptions.Default)
+        {
+            var contextEx = context as ITrackingIndexingContext;
+            if (contextEx == null)
+            {
+                base.Update(context, indexableUniqueId, operationContext, indexingOptions);
+                return;
+            }
+            if (this.IsExcludedFromIndex(indexableUniqueId, operationContext, true))
+                return;
+            base.Update(context, indexableUniqueId, operationContext, indexingOptions);
+        }
+    }
+}
---
+++ src/Sitecore.Support.104142.108165/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.104142.108165")]
+[assembly: AssemblyProduct("Sitecore.Support.104142.108165")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("The search index uses only the first crawler that is assigned to it and ignores any other crawlers")]
+
