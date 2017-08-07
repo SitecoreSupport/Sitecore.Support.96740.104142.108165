9fc9e712371b20d731c5b31255d5c4f5a6c99a6b
DK\psk <psk@sitecore.net>
2017-08-07 17:31:35 -0400

    Refactoring. Add fix for #96740: dependencies should not be skipped if referring item is excluded from indexing.

--- src/Sitecore.Support.104142.108165/ContentSearch/SitecoreItemCrawler.cs
+++ src/Sitecore.Support.104142.108165/ContentSearch/SitecoreItemCrawler.cs
@@ -1,23 +1,29 @@
-﻿using System.Collections.Concurrent;
-
-namespace Sitecore.Support.ContentSearch
+﻿namespace Sitecore.Support.ContentSearch
 {
     using Sitecore.ContentSearch;
+    using System.Collections.Concurrent;
+    using Sitecore.ContentSearch.Pipelines.GetDependencies;
     public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
     {
         protected ConcurrentDictionary<IIndexableUniqueId, object> Processed;
         public override void Update(IProviderUpdateContext context, IIndexableUniqueId indexableUniqueId,
             IndexEntryOperationContext operationContext, IndexingOptions indexingOptions = IndexingOptions.Default)
         {
-            var contextEx = context as ITrackingIndexingContext;
-            if (contextEx == null)
+            if (!this.IsExcludedFromIndex(indexableUniqueId, operationContext, true))
             {
                 base.Update(context, indexableUniqueId, operationContext, indexingOptions);
-                return;
             }
-            if (this.IsExcludedFromIndex(indexableUniqueId, operationContext, true))
-                return;
-            base.Update(context, indexableUniqueId, operationContext, indexingOptions);
+        }
+
+        protected override void UpdateDependents(IProviderUpdateContext context, SitecoreIndexableItem indexable)
+        {
+            foreach (IIndexableUniqueId current in indexable.GetIndexingDependencies())
+            {                
+                if (!this.IsExcludedFromIndex(current, true))
+                {
+                    this.Update(context, current, IndexingOptions.Default);
+                }
+            }
         }
     }
 }
--- src/Sitecore.Support.104142.108165/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.104142.108165/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.104142.108165")]
-[assembly: AssemblyProduct("Sitecore.Support.104142.108165")]
+[assembly: AssemblyTitle("Sitecore.Support.96740.104142.108165")]
+[assembly: AssemblyProduct("Sitecore.Support.96740.104142.108165")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyDescription("The search index uses only the first crawler that is assigned to it and ignores any other crawlers")]
+[assembly: AssemblyDescription("The search index uses only the first crawler that is assigned to it and ignores any other crawlers. Dependencies are not indexed if referring item is exluded from indexing.")]
 
